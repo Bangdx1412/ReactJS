847adbcffc4917e52b95416f253c82a5b84ccbb8
Bangdx1412 <email_lin_kt_vi_Bangdx1412>
2025-02-11 13:22:56 +0700

    bai toan checkbox

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
 
+// Response tu API tra ve
+const courses = [
+  { id: 1, name: "HTML,CSS" },
+  { id: 2, name: "Js" },
+  { id: 3, name: "Reactjs" },
+];
 function App() {
   // One way byding
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const [checked, setChecked] = useState(2);
 
-  const handleSubmit = () => {
-    console.log({ name, email });
-  };
+  const handleSubmit = () => {};
+  console.log(checked);
 
   return (
     <div style={{ padding: "20px" }}>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <input value={email} onChange={(e) => setEmail(e.target.value)} />
+      {courses.map((cour) => (
+        <div key={cour.id}>
+          <input
+            type="radio"
+            checked={checked === cour.id}
+            onChange={() => setChecked(cour.id)}
+          />
+          {cour.name}
+        </div>
+      ))}
       <button onClick={handleSubmit}>Register</button>
     </div>
   );
