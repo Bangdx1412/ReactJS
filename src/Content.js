8fc44c88c32b76187314f8e6afa90408962ab139
Bangdx1412 <email_lin_kt_vi_Bangdx1412>
2025-02-13 23:13:06 +0700

    vidu t2

--- src/Content.js
+++ src/Content.js
@@ -36,62 +36,21 @@ import { useEffect, useState } from "react";
 // 1. Callback luôn được gọi Sau khi component mounted
 // 2. Cleanup function sẽ được gọi trước khi component bị unmounted
 
-const tabs = ["posts", "comments", "albums"];
 function Content() {
-  const [title, setTitle] = useState("");
-  const [posts, setPosts] = useState([]);
-  const [type, setType] = useState("posts");
-  const [showButton, setShowButton] = useState(false);
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((res) => res.json())
-      .then((posts) => {
-        setPosts(posts);
-      });
-  }, [type]);
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY >= 200) {
-        // Hien nut button
-        setShowButton(true);
-      } else {
-        // An nut button
-        setShowButton(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    console.log("add");
+  const [chieuDai, setChieuDai] = useState(window.innerWidth);
 
-    // Cleanup function
+  useEffect(() => {
+    const hanldeResize = () => {
+      setChieuDai(window.innerWidth);
+    };
+    window.addEventListener("resize", hanldeResize);
+
     return () => {
-      window.removeEventListener("scroll", handleScroll);
-      console.log("remove");
+      window.removeEventListener("resize", hanldeResize);
     };
   }, []);
-  return (
-    <div>
-      {tabs.map((tab) => (
-        <button
-          onClick={() => setType(tab)}
-          key={tab}
-          style={type === tab ? { color: "fff", backgroundColor: "#333" } : {}}
-        >
-          {tab}
-        </button>
-      ))}
-      <input value={title} onChange={(e) => setTitle(e.target.value)} />
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-      {showButton && (
-        <button style={{ position: "fixed", right: 20, bottom: 20 }}>
-          Go to top
-        </button>
-      )}
-    </div>
-  );
+
+  return <div>{chieuDai}</div>;
 }
 
 export default Content;
