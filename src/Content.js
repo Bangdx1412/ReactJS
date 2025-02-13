43ea6a97d681162ce968561646c2f8bcfd1a807e
Bangdx1412 <email_lin_kt_vi_Bangdx1412>
2025-02-13 22:17:27 +0700

    TH3

--- src/Content.js
+++ src/Content.js
@@ -29,23 +29,35 @@ import { useEffect, useState } from "react";
 // 2. useEffect(callback,[])
 // - Chỉ gọi callback 1 lần sau khi component được mounted
 // 3. useEffect(callback,[dependency ])
+//  - Callback sẽ được gọi lại mỗi khi dependency thay đổi
 
 // ----------------------
 
 // 1. Callback luôn được gọi Sau khi component mounted
 
+const tabs = ["posts", "comments", "albums"];
 function Content() {
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
+  const [type, setType] = useState("posts");
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((res) => res.json())
       .then((posts) => {
         setPosts(posts);
       });
-  }, []);
+  }, [type]);
   return (
     <div>
+      {tabs.map((tab) => (
+        <button
+          onClick={() => setType(tab)}
+          key={tab}
+          style={type === tab ? { color: "fff", backgroundColor: "#333" } : {}}
+        >
+          {tab}
+        </button>
+      ))}
       <input value={title} onChange={(e) => setTitle(e.target.value)} />
       <ul>
         {posts.map((post) => (
